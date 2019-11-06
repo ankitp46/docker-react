c9b69ec8a22d11dae56a3b08a5bc3fa4a1129018
root <root@ip-172-31-44-223.us-east-2.compute.internal>
2019-11-06 17:14:44 +0000

    FEATURE COMMIT

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         HELLLLOOOOO Ankitt  There!
+         FEATURE BRANCHH!!
         </p>
         <a
           className="App-link"
